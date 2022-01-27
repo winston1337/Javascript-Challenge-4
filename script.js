a6c221977137a76273c71c526369968768052e4b
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-01-27 11:48:08 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var greeting = "hello world"
+console.log(greeting)
+// EASY: Write a function that takes in an array of numbers and outputs the average of all the numbers. 
+
+
+
+
+// MEDIUM: Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+
+
+
+//HARD: Create two buttons, one that when clicked turns the background of the body red. The other, when clicked, turns the body's background color white. Each of the background colors should be defined as class styles and when the buttons are pushed the body's classList is updated to include the correct class for the background. 
+
+
+
+
+//*VERY HARD: You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
